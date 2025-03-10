cc85780c865cdf1da3af8c4022dbcdf32909c7f7
Elisa Bavaresco <bavaresco.elisa@gmail.com>
2025-03-10 16:37:16 -0300

    comentários adicionados:

--- inputs_numeros.py
+++ inputs_numeros.py
@@ -14,3 +14,6 @@ vendas_dia1 = float(input("Vendas Dia 1: "))
 vendas_dia2 = float(input("Vendas Dia 2: "))
 
 print(f"Total de Vendas: {vendas_dia1 + vendas_dia2}")
+
+# ou, colocar o float/int no print e nao da declaração da variável
+# print(f"Total de Vendas: {float(vendas_dia1) + float(vendas_dia2)}")
