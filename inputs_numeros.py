7795b861a0571c7ea14a8ee3c24484d7860c2551
Elisa Bavaresco <bavaresco.elisa@gmail.com>
2025-03-10 16:24:33 -0300

    estudo sobre input numéricos

---
+++ inputs_numeros.py
@@ -0,0 +1,16 @@
+# 1 exemplo
+
+vendas = input("Digite suas vendas do dia: ")
+vendas = float(vendas)
+
+# ou
+# vendas = float(input("Digite suas vendas do dia: "))
+
+bonus = vendas * 0.01
+print(bonus)
+
+# 2 exemplo
+vendas_dia1 = float(input("Vendas Dia 1: "))
+vendas_dia2 = float(input("Vendas Dia 2: "))
+
+print(f"Total de Vendas: {vendas_dia1 + vendas_dia2}")
