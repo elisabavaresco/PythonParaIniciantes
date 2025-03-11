c61a556d276d574ad0efd2e27e5867792d64c927
Elisa Bavaresco <bavaresco.elisa@gmail.com>
2025-03-11 14:16:36 -0300

    edição na estrutura de condicionamento

--- CalculoDaTaxaDeEngajamento.py
+++ CalculoDaTaxaDeEngajamento.py
@@ -6,11 +6,9 @@ engajamento = ((likes + comentarios)/seguidores)
 
 if engajamento <= 0.01:
     print(f"A taxa de engajamento é {engajamento:.2%}, o que é abaixo da média")
-
-if engajamento > 0.01 and engajamento < 0.05:
+elif engajamento > 0.01 and engajamento < 0.05:
     print(f"A taxa de engajamento é {engajamento:.2%}, o que é dentro da média")
-
-if engajamento >= 0.05:
+elif engajamento >= 0.05:
     print(f"A taxa de engajamento é {engajamento:.2%}, o que é acima da média")
 
 print("Fim")
