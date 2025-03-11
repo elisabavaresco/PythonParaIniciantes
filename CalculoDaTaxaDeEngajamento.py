6c57c0d70a4bd5c1ad0c61cc1613d18bc325321c
Elisa Bavaresco <bavaresco.elisa@gmail.com>
2025-03-11 13:46:46 -0300

    adicionando condicionamento na resposta

--- CalculoDaTaxaDeEngajamento.py
+++ CalculoDaTaxaDeEngajamento.py
@@ -3,5 +3,14 @@ likes = int(input("Número de likes: "))
 comentarios = int(input("Número de comentários: "))
 
 engajamento = ((likes + comentarios)/seguidores)
-   
-print(f"A sua taxa de engajamento é: {engajamento:.1%}")
+
+if engajamento <= 0.01:
+    print(f"A taxa de engajamento é {engajamento:.2%}, o que é abaixo da média")
+
+if engajamento > 0.01 and engajamento < 0.05:
+    print(f"A taxa de engajamento é {engajamento:.2%}, o que é dentro da média")
+
+if engajamento >= 0.05:
+    print(f"A taxa de engajamento é {engajamento:.2%}, o que é acima da média")
+
+print("Fim")
