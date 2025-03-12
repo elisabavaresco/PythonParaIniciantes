51105558ca0b3c6fa358aa46c844546e6e7f1d02
Elisa Bavaresco <bavaresco.elisa@gmail.com>
2025-03-12 14:27:40 -0300

    Exercicio de construção de sistema de consulta de preço

---
+++ exercicios02.py
@@ -0,0 +1,19 @@
+# Exercíco desafio
+# Criar um sistema de consulta de preço
+
+precos = [1500, 1000, 800, 2000]
+produtos = ["celular", "camera", "fone de ouvido", "monitor"]
+
+# quero que o usuário me dê um input e o meu sistema diga qual o preço do produto
+# se o produto não existir, o programa precisa dizer pro usuário que esse produto não existe
+
+produto_buscado = input("Digite aqui o produto que gostaria de consultar: ")
+produto_buscado = produto_buscado.lower()
+
+if produto_buscado in produtos:
+        encontrar_preco = produtos.index(produto_buscado)
+        print(precos[encontrar_preco])
+else:
+        print("Produto não cadastrado")
+        print(f"Escolha um dos produtos cadastrados: {produtos}")
+print ("Fim")
