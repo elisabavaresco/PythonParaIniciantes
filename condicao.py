4df6929804d70e9206066ec9785e7684f1230674
Elisa Bavaresco <bavaresco.elisa@gmail.com>
2025-03-12 14:42:14 -0300

    salvamento

--- condicao.py
+++ condicao.py
@@ -1,16 +1,16 @@
 # aprendendo a construir a estrutura do if
 
-# faturamento = 1000
-# custo = 8100
+faturamento = 1000
+custo = 8100
 
-# lucro = faturamento - custo
+lucro = faturamento - custo
 
-# if  lucro >= 0: # if + condição
-#     # aqui você vai escrever tudo que quer que aconteça se a condição for VERDADEIRA
-#     print(lucro)
-# else: 
-#     # aqui você coloca tudo que você quer que aconteça se a condição for FALSA
-#     print("Prejuízo", lucro)
+if  lucro >= 0: # if + condição
+    # aqui você vai escrever tudo que quer que aconteça se a condição for VERDADEIRA
+    print(lucro)
+else: 
+    # aqui você coloca tudo que você quer que aconteça se a condição for FALSA
+    print("Prejuízo", lucro)
 # != 'diferente de'
 # > 'maior que'
 # < 'menor que'
@@ -18,17 +18,17 @@
 # <= 'menor ou igual a'
 # == 'igual a'
 
-# produtos = ["iphone", "ipad", "airpod"]
-# novo_produto = input("Digite aqui o produto: ")
-# novo_produto = novo_produto.lower() # tratamento do input do usuário
+produtos = ["iphone", "ipad", "airpod"]
+novo_produto = input("Digite aqui o produto: ")
+novo_produto = novo_produto.lower() # tratamento do input do usuário
 
-# if novo_produto in produtos:
-#     print("Produto já cadastrado")
-# else:
-#     print("Produto cadastrado com sucesso")
-#     produtos.append(novo_produto) # adicioando o novo produto à lista de produtos
+if novo_produto in produtos:
+    print("Produto já cadastrado")
+else:
+    print("Produto cadastrado com sucesso")
+    produtos.append(novo_produto) # adicioando o novo produto à lista de produtos
 
-# print(produtos)
+print(produtos)
 
 # Segundo exemplo - faixas
 # acima de 15000 -> 500 de bonus
@@ -87,14 +87,14 @@ print(bonus)
 
 # opção 2
 
-if not vendas_empresa > meta_empresa:
+if not vendas_empresa > meta_empresa: # negando uma condição
     bonus = 0
 else:
-    if vendas > 15000 and vendas_empresa > meta_empresa:
+    if vendas > 15000:
         bonus = 500
-    elif vendas > 10000 and vendas_empresa > meta_empresa: 
+    elif vendas > 10000: 
         bonus = 150
-    elif vendas > 5000 and vendas_empresa > meta_empresa:
+    elif vendas > 5000:
         bonus = 50
     else:
         bonus = 0
