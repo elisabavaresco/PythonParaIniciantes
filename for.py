446a8cbfc9dcb919795645e6146f2fcb2e03231b
Elisa Bavaresco <bavaresco.elisa@gmail.com>
2025-03-19 16:02:42 -0300

    exercicio 2

--- for.py
+++ for.py
@@ -4,7 +4,6 @@ for i in range(10): # para executar algo X vezes
     print("Se inscreva")
 
 lista_precos = [1500, 100, 800, 2000]
-imposto = 0.1
 
 for preco in lista_precos: #vai permitir percorrer uma lista e executar algo para cada item até ela acabar
     imposto = preco * 0.1
@@ -19,5 +18,17 @@ for preco in lista_precos:
         imposto = preco * 0.1
     else:
         imposto = preco * 0.15
-        
     print(f"Preco: {preco}, Imposto: {imposto}")
+
+# exercio 2
+# calcular o total de imposto 
+
+total_imposto = 0 # acumulado
+for preco in lista_precos:
+    if preco <= 1000:
+        imposto = preco * 0.1
+    else:
+        imposto = preco * 0.15
+    print(f"Preco: {preco}, Imposto: {imposto}")
+    total_imposto = total_imposto + imposto
+print(f"O total de imposto é: {total_imposto}")
