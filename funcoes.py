2cf8856d5fbcb14cd09ae2e7b3c104f1a9e3abdc
Elisa Bavaresco <bavaresco.elisa@gmail.com>
2025-03-24 10:06:37 -0300

    adicionando parâmetros à função de calculo de imposto

--- funcoes.py
+++ funcoes.py
@@ -1,10 +1,12 @@
-lista_precos = [1500, 1000, 800, 2000]
+lista_precos = [1500, 1000, 800, 3000]
 
 total_imposto = 0
 
 def calcular_imposto(preco): # entre parênteses está o parâmetro da função
-    if preco > 1000:
-        imposto = preco * 0.15 # as variáveis criadas dentro da função, só existe dentro da função
+    if preco > 2000:
+        imposto = preco * 0.2 # as variáveis criadas dentro da função, só existe dentro da função
+    elif preco > 1000:
+        imposto = preco * 0.15
     else:
         imposto = preco * 0.1
     print(f"Preco: {preco}, Imposto: {imposto}")
@@ -12,7 +14,31 @@ def calcular_imposto(preco): # entre parênteses está o parâmetro da função
 
 for preco in lista_precos:
     imposto = calcular_imposto(preco)
+    total_imposto = total_imposto + imposto
 
+print(total_imposto)
+
+def se_inscrever(): # definindo/ criando funções
+    print("Se inscreve no canal")
+
+# adicionando parâmetros
+
+lista_precos = [1500, 1000, 800, 3000]
+
+total_imposto = 0
+
+def calcular_imposto(preco, aliquota1, aliquota2, aliquota3): # é possível solicitar mais de um parâmetro
+    if preco > 2000:
+        imposto = preco * aliquota1 
+    elif preco > 1000:
+        imposto = preco * aliquota2
+    else:
+        imposto = preco * aliquota3
+    print(f"Preco: {preco}, Imposto: {imposto}")
+    return imposto # para uma variável existir fora de uma função, é preciso "retornar" a variável
+
+for preco in lista_precos:
+    imposto = calcular_imposto(preco, 0.2, 0.15, 0.1)
     total_imposto = total_imposto + imposto
 
 print(total_imposto)
