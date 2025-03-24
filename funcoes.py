ec6d4074b9247b854453ec208aa39c6a834a3fea
Elisa Bavaresco <bavaresco.elisa@gmail.com>
2025-03-24 10:14:04 -0300

    aprendendo a definir valores padrões para os parâmetros

--- funcoes.py
+++ funcoes.py
@@ -35,7 +35,7 @@ def calcular_imposto(preco, aliquota1, aliquota2, aliquota3): # é possível sol
     else:
         imposto = preco * aliquota3
     print(f"Preco: {preco}, Imposto: {imposto}")
-    return imposto # para uma variável existir fora de uma função, é preciso "retornar" a variável
+    return imposto 
 
 for preco in lista_precos:
     imposto = calcular_imposto(preco, 0.2, 0.15, 0.1)
@@ -43,5 +43,25 @@ for preco in lista_precos:
 
 print(total_imposto)
 
-def se_inscrever(): # definindo/ criando funções
-    print("Se inscreve no canal")
+# definindo valores padrões para os parâmetros 
+
+lista_precos = [1500, 1000, 800, 3000]
+
+total_imposto = 0
+
+def calcular_imposto(preco, aliquota1=0.2, aliquota2=0.15, aliquota3=0.1): # é possível definir valores padrões para os parâmetros de uma função
+    if preco > 2000:
+        imposto = preco * aliquota1 
+    elif preco > 1000:
+        imposto = preco * aliquota2
+    else:
+        imposto = preco * aliquota3
+    print(f"Preco: {preco}, Imposto: {imposto}")
+    return imposto 
+
+for preco in lista_precos:
+    imposto = calcular_imposto(preco) # se for usar só o padrão dos parâmetros, não é preciso informar o valor deles ao chamar a função, ou, se quiser alterar, basta seguir o exemplo a baixo:
+#   imposto = calcular_imposto(preco, aliquita1=0.25)
+    total_imposto = total_imposto + imposto
+
+print(total_imposto)
