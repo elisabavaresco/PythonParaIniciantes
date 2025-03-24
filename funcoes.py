aa9b88b53f7009a816c90e8236575d1e9711996f
Elisa Bavaresco <bavaresco.elisa@gmail.com>
2025-03-24 09:58:22 -0300

    definindo função para calculo do imposto

--- funcoes.py
+++ funcoes.py
@@ -2,12 +2,17 @@ lista_precos = [1500, 1000, 800, 2000]
 
 total_imposto = 0
 
-for preco in lista_precos:
+def calcular_imposto(preco): # entre parênteses está o parâmetro da função
     if preco > 1000:
-        imposto = preco * 0.15
+        imposto = preco * 0.15 # as variáveis criadas dentro da função, só existe dentro da função
     else:
         imposto = preco * 0.1
     print(f"Preco: {preco}, Imposto: {imposto}")
+    return imposto # para uma variável existir fora de uma função, é preciso "retornar" a variável
+
+for preco in lista_precos:
+    imposto = calcular_imposto(preco)
+
     total_imposto = total_imposto + imposto
 
 print(total_imposto)
