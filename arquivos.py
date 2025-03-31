9cbda8b4b21c6727fd21c71b1d48e374b7d2b98b
Elisa Bavaresco <bavaresco.elisa@gmail.com>
2025-03-31 09:33:17 -0300

    aprendendo a ler e tratar arquivos de texto

--- arquivos.py
+++ arquivos.py
@@ -12,4 +12,17 @@ arquivo.close()
 
 with open("vendas.txt", "r") as arquivo: # criei a variável 'arquivo'
     # e vai fazer aqui dentro o que quiser com o arquivo, depois ele automaticamente vai fechar esse arquivo
-    
+    # infos = arquivo.read() # isso aqui é uma string, o .read() vai te retornar como um texto tudo junto
+    infos = arquivo.readlines() # vai te retornar uma lista, em que cada item da lista é uma linha
+
+# para calcular o total de vendas das infos trazidas pelo arquivo:
+
+vendas_totais = 0 
+for item in infos:
+    item = item.replace("\n", "")
+    item = item.replace(" ", "")
+    resultado = item.split(",") # vai separar o 'texto' pela virgula e vai entregar uma lista dos itens separados
+    valor = resultado[1] # esse cara aqui ainda está em formato de texto
+    valor = float(valor) # transformando o texto em número
+    vendas_totais = vendas_totais + valor
+print (vendas_totais)
