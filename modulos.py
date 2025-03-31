c993b4afdf5de8aeb981dc36a559aec94b8ec8fc
Elisa Bavaresco <bavaresco.elisa@gmail.com>
2025-03-31 10:00:25 -0300

    aprendendo sobre bibliotecas

---
+++ modulos.py
@@ -0,0 +1,9 @@
+# o python tem varias bibliotecas que permitem que você execute tarefas de forma eficiente
+
+# vamos aprender sobre as bibliotecas os e requests
+
+import os # essa biblioteca permite que a gente acesse as coisas do nosso computador
+
+arquivos = os.listdir() # para listar tudo que está no diretório
+
+os.rename("vendas.txt", "arquivos/vendas.txt") # para mudar um arquivo de lugar
